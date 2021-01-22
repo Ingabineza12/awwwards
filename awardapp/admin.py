04a80a2703989967bc95b349e8840229d6aa7398
Deborah M Ingabire <ingabineza@gmail.com>
2021-01-22 14:44:54 +0200

    added admin.py

--- awardapp/admin.py
+++ awardapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile,Comment,Project,Rates
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Rates)
+admin.site.register(Project)
