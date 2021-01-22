deaf0d2ced6c86d1f72eb67bc2d07e20b9b2d213
Deborah M Ingabire <ingabineza@gmail.com>
2021-01-22 13:48:13 +0200

    added project urls.py

--- award/urls.py
+++ award/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+     url(r'',include('awardapp.urls')),
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^api-token-auth/', obtain_auth_token),
 ]
