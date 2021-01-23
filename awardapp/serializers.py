a2942be4890aeb8f3b4309259e3dcfc9b6fdf409
Deborah M Ingabire <ingabineza@gmail.com>
2021-01-23 16:03:51 +0200

    modified serializers

--- awardapp/serializers.py
+++ awardapp/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from .models import Project,Profile
 
-class MerchSerializer(serializers.ModelSerializer):
+class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ('title', 'description', 'link','image')
 
-class MerchSerializerProfile(serializers.ModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ('profile_pic', 'bio', 'contact')
+        fields = ('profile_pic', 'bio')
